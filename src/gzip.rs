e97d7a8a3cbeb3285eed241b76da462f19c6088e
Alejandro González <AlexTMjugador@users.noreply.github.com>
2021-05-03 14:37:37 +0200

    Add streaming support
    These changes add streaming support to the crate, so entire files no
    longer have to be copied to main memory before being compressed. This
    streaming support is safer and easier to use than memmapping the file, a
    operation that is not entirely OS-agnostic and requires detailed
    analysis of how other processes may interact with the memmapped file to
    guarantee stability and safety. The crate now accepts any Read trait
    implementation for input data, which provides lots of flexibility.
    
    Compression should be unaffected, as the input data is still read in
    blocks of ZOPFLI_MASTER_BLOCK_SIZE like before, and a sliding window of
    the last ZOPFLI_WINDOW_SIZE bytes read is kept to initialize the
    dictionary for each master block. To test that the changes work okay, I
    ran the test/run.sh script, which compressed the test files with no
    issues. I got the original idea for this improvement from
    https://github.com/google/zopfli/issues/14#issuecomment-77830531.
    
    Although I tried to follow the style of the code that already exists and
    not introduce too much modern Rust features, MSRV is now bumped to 1.41.
    Also, the public interface exposed by this crate changed in a
    backwards-incompatible manner, but the crate major version is still 0,
    and semantic versioning conventions tell that public APIs in major
    version 0 are unstable, so I don't think this should be a problem.
    
    Resolves #18.

--- src/gzip.rs
+++ src/gzip.rs
@@ -1,9 +1,10 @@
-use std::io::{self, Write};
-use crc::crc32;
+use std::io::{self, Read, Write};
+use crc::{Hasher32, crc32};
 use byteorder::{LittleEndian, WriteBytesExt};
 
 use deflate::{deflate, BlockType};
 use Options;
+use iter_read::IterRead;
 
 static HEADER: &'static [u8] = &[
     31,  // ID1
@@ -21,13 +22,36 @@ static HEADER: &'static [u8] = &[
 ];
 
 /// Compresses the data according to the gzip specification, RFC 1952.
-pub fn gzip_compress<W>(options: &Options, in_data: &[u8], mut out: W) -> io::Result<()>
-    where W: Write
+pub fn gzip_compress<R, W>(options: &Options, in_data: R, insize: u64, mut out: W) -> io::Result<()>
+    where R: Read, W: Write
 {
+    let mut crc_digest = crc32::Digest::new(crc32::IEEE);
+    let mut read_error_kind = None;
+
+    let in_data = IterRead::new(in_data.bytes().filter_map(|byte_result| {
+        read_error_kind = byte_result.as_ref().map_or_else(
+            |error| Some(error.kind()),
+            |byte| {
+                crc_digest.write(&[*byte]);
+                None
+            }
+        );
+
+        byte_result.ok()
+    }).fuse());
+
     try!(out.by_ref().write_all(HEADER));
 
-    try!(deflate(options, BlockType::Dynamic, in_data, out.by_ref()));
+    try!(deflate(options, BlockType::Dynamic, in_data, insize, out.by_ref()));
 
-    try!(out.by_ref().write_u32::<LittleEndian>(crc32::checksum_ieee(in_data)));
-    out.write_u32::<LittleEndian>(in_data.len() as u32)
+    // in_data is fused and stops reading bytes after the first error, so
+    // this if is evaluated as soon as an error occurs. The deflate function
+    // has received EOF at this point, so the last block has been written.
+    if let Some(error_kind) = read_error_kind {
+        return Err(error_kind.into());
+    }
+
+    try!(out.by_ref().write_u32::<LittleEndian>(crc_digest.sum32()));
+
+    out.write_u32::<LittleEndian>(insize as u32)
 }
